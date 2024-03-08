5ad1a9bf7c691bfa94ac5361bcbe203d78331fc9
Gustavo <gustavolsilva@outlook.com>
2024-03-08 14:20:58 -0300

    Inclusão: Exemplo00.py com informações e exemplos durante a aula.

---
+++ exemplo00.py
@@ -0,0 +1,49 @@
+# %%
+print("Alguma coisa!")
+# %%
+# Você consegue fazer Operações:
+
+# Soma
+print(3 + 5)
+
+# Multiplicação
+print(2 * 2)
+
+# Divisao
+print( 15 / 5 )
+
+# Exponeciação
+print(2 ** 2)
+
+# %%
+
+# Concatenação de tipos iguais
+
+print("Ola" + " "+ "Turma!")
+
+# %%
+nome = input("Digite o seu nome: ")
+print("Ola " + nome)
+
+# %%
+7 != '7' != 'sete'
+# %%
+numero = 3 #int
+numero_decimal = 4.78 #float
+nome_de_usuario = "Gustavo" #string
+verdadeiro = True # booleano
+
+SENHA_AWS = 1234156 #Constante
+_SENHA_AWS = 123456 #Secreta
+
+# %%
+print(type(numero))
+# %%
+numero = 3
+print(numero)
+
+numero = 4
+print(numero)
+
+numero = "Python"
+print(numero)
